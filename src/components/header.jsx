e603233f7ef4a1b3c69d1637df2ae8639a51662a
Chris Fryer <cfryerdev@gmail.com>
2021-03-22 11:59:32 -0600

    Updated nav title

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ export default ({ onToggleTheme }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <NavLink className="navbar-brand" to="/">
-        <i className="fas fa-chart-bar fa-lg pr-2"></i> Report Viewer
+        <i className="fas fa-chart-bar fa-lg pr-2"></i> Artillery Report Viewer
       </NavLink>
       <span className="float-right mt-2">
         <a alt="Home" target="blank" href="https://artillery.io">
